af84f4d17a0d0caeb3193298faf108bd9cd09c50
unknown <udivyy.99@gmail.com>
2021-05-04 10:57:55 +0530

    delete task

---
+++ src/component/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = ({ title }) => {
+
+    const onClick = () => {
+        console.log('click');
+    }
+
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='steelblue' text='Add' onClick={onClick} />
+        </header>
+    )
+}
+
+// const styles = {
+//     color: 'blue',
+//     backgroundColor: 'black'
+// }
+
+export default Header
